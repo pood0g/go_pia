8cf15bcea19151a1f2e85eff2a962230c47dde85
pood0g <>
2023-05-21 23:22:09 +1000

    Moved Constants to own file

--- piafuncs.go
+++ piafuncs.go
@@ -9,13 +9,6 @@ import (
 	"sort"
 )
 
-// String constants used within the application.
-const REGION_URL string = "https://serverlist.piaservers.net/vpninfo/servers/v6"
-const TOKEN_URL string = "https://www.privateinternetaccess.com/api/client/v2/token"
-const CT_FORM string = "application/x-www-form-urlencoded"
-
-// CA file for PIA server trust
-const PIA_CERT string = "./ca.rsa.4096.crt"
 
 // function to get and parse region JSON data
 func getPIAServerData() RegionData {
